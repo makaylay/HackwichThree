3e2245babdcfc4798a2878d20a417d4480a790ac
makayla <mcsyanos@hawaii.edu>
2018-02-05 10:48:58 -1000

    Inserted 3 "About" icons into the Assets Folder and applied the "About" icon to the tab bar

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   //set first label to string "About"
+       self.firstLabel.text = "About"
+        
+    
     }
 
     override func didReceiveMemoryWarning() {
