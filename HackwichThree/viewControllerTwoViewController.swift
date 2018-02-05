71fa6798b980778484d9a4c87232c1d12c00681e
makayla <mcsyanos@hawaii.edu>
2018-02-05 10:20:28 -1000

    Added two labels and created custom classes

---
+++ HackwichThree/viewControllerTwoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  viewControllerTwoViewController.swift
+//  HackwichThree
+//
+//  Created by CM Student on 2/5/18.
+//  Copyright © 2018 UHWO. All rights reserved.
+//
+
+import UIKit
+
+class viewControllerTwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
