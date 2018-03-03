562e46d5933e50ddddea7602150e84c7d156a0c9
Makayla <mcsyanos@hawaii.edu>
2018-03-02 22:47:25 -1000

    Completed PJ2 About Me Tab

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -11,12 +11,33 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet var secondLabel: UILabel!
+    @IBOutlet var thirdLabel: UILabel!
+    @IBOutlet var fourthLabel: UILabel!
+    @IBOutlet var fifthLabel: UILabel!
+    @IBOutlet var textViewLabel: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-   //set first label to string "About"
-       self.firstLabel.text = "About"
+  
+        //set first label to string "Name"
+        self.firstLabel.text = "My Name is:"
         
+        //set second label to string "Major"
+        self.secondLabel.text = "My Major is:"
+        
+        //set third label to string "Makayla"
+        self.thirdLabel.text = "Makayla"
+        
+        //set fourth label to string "Humanities"
+        self.fourthLabel.text = "Humanities- Creative Media"
+        
+        //set fifth label to string "Classes"
+        self.fifthLabel.text = "My Spring 2018 Classes:"
+        
+        //set text view label to "Spring Classes"
+        self.textViewLabel.text = "CM 120- Intro to Digital Vido, CM 161- Intro to iOS Mobile Application Development, ENG 441- Gender & Sexuality in Lit/Film, HIST 151- World History since 1500, HUM 300- Humanities Seminar"
     
     }
 
